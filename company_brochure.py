26a8e9ead3cab70c141f1602fdd7de530634194f
Yasir-Khan-7 <110191648+Yasir-Khan-7@users.noreply.github.com>
2026-03-14 22:46:05 +0500

    setup the enviroment variable

---
+++ company_brochure.py
@@ -0,0 +1,26 @@
+
+# Create a product that builds a Brochure for a company to be used for prospective clients, investors and potential recruits.
+# We will be provided a company name and their primary website."
+
+#import neccesry libraries
+import os 
+from openai import OpenAI
+from dotenv import load_dotenv
+
+
+#initialization and constants
+
+load_dotenv(override=True)
+
+api_key = os.getenv("GROQ_API_KEY")
+
+
+if api_key and api_key.startswith("gsk_"):
+    print("API key is set correctly.")
+else:
+    print("API key is not set correctly. Please check your .env file.")
+
+
+
+
+

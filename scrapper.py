2b5b085153c2e5e54387f0b3d6ce41954a6d134e
Yasir-Khan-7 <110191648+Yasir-Khan-7@users.noreply.github.com>
2026-03-23 23:45:50 +0500

    updated the scrapper

--- scrapper.py
+++ scrapper.py
@@ -1,13 +1,16 @@
 #this file serves as scrapper which will help in scrapping the data from the website 
 
-
+# It uses the requests library to fetch the content of the website and BeautifulSoup to parse the HTML and extract the relevant information. The scrape_content function takes a URL as input and returns the title and body text of the webpage, while the fetch_website_links function returns a list of all the links found on the webpage.
 import requests
 from bs4 import BeautifulSoup
 
+# Define headers to mimic a browser request
 headers= {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 } 
 
+
+# Function to scrape content from a given URL
 def scrape_content(url):
 
 
@@ -29,7 +32,7 @@ def scrape_content(url):
         return (title + "\n\n"  +text)[:2000]
 
 
-
+# Function to fetch all links from a given URL
 def fetch_website_links(url):
 
 
